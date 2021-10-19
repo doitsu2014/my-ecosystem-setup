6bf49729b5b4e230955fd046986172829909ba06
Duc Tran <ducth@doitsu.tech>
2021-10-19 09:08:11 +0700

    latest

--- swarm/infrastructure/start.sh
+++ swarm/infrastructure/start.sh
@@ -1,4 +1,4 @@
 docker stack deploy -c docker-compose-initialize.yml \
 	-c docker-compose-es-cluster.yml \
-	-c docker-compose-postgres.yml \
+	-c docker-compose-postgres-cluster.yml \
 	-c docker-compose-kafka-cluster.yml infrastructure
